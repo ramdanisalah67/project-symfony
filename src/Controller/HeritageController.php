8d9fa1be11c222a8c8d4517dd408dc09df59626d
ramdanisalah67 <ramdanisalah67@gmail.com>
2023-03-19 21:34:15 +0000

    integration templates et heritage

---
+++ src/Controller/HeritageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HeritageController extends AbstractController
+{
+    #[Route('/twig', name: 'app_heritage')]
+    public function index(): Response
+    {
+        return $this->render('heritage/index.html.twig', [
+            'controller_name' => 'HeritageController',
+        ]);
+    }
+
+
+    #[Route('/twig/heritage', name: 'heritage')]
+    public function heritage(): Response
+    {
+        return $this->render('heritage.html.twig');
+    }
+}
---
+++ src/Controller/TemplateController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TemplateController extends AbstractController
+{
+    #[Route('/template', name: 'app_template')]
+    public function index(): Response
+    {
+        return $this->render('template/index.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+#[Route('/template/static', name: 'st')]
+    public function static(): Response
+    {
+        return $this->render('hello.html.twig', [
+            'controller_name' => 'TemplateController',
+        ]);
+    }
+
+}
