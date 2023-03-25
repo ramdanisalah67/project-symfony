e2e847eb840d4a137a96113cca2933a02ac3e51a
ramdanisalah67 <ramdanisalah67@gmail.com>
2023-03-25 04:15:51 +0000

    symfony findby and pagination

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -30,23 +30,45 @@ class PersonneController extends AbstractController
 
     }
 
+    #[Route('/personne/All',name:'personne.All')]
+    public function findAll(ManagerRegistry $doctrine):Response{
+        $repository = $doctrine->getRepository(Personne::class);
+        $personnes = $repository->findBy(['firstName'=>'y%'],['firstName'=>'ASC'],2,1);
+        if(!$personnes) {
+            $this->addFlash('error', "la personne qui vous êtes chercher n'existe pas");
+            return  $this->redirectToRoute('personne.list') ;
+        }
+
+        return $this->render('personne/index.html.twig',['personnes'=>$personnes]);
+
+    }
+    #[Route('/personne/All2/{page?1}/{nbr?12}',name:'personne.All2')]
+    public function findAll2(ManagerRegistry $doctrine,$nbr,$page):Response{
+        $repository = $doctrine->getRepository(Personne::class);
+        $personnes = $repository->findBy([],['firstName'=>'DESC'],$nbr,($page-1)*$nbr);
 
 
-/*
+        return $this->render('personne/index.html.twig',['personnes'=>$personnes]);
 
-    #[Route('/add', name: 'app_personne')]
-    public function addPersonne(ManagerRegistry$doctrine ): Response
-    {
-        //$this->getDoctrine():version Sf<=5
+    }
 
-      $entityManager = $doctrine->getManager();
 
-        //Ajouter operation d'insertion dans ma transaction
-        $entityManager->persist($personne);
-        //Execute la transaction
-        $entityManager->flush();
-        return $this->render('personne/details.html.twig', [
-            'personne_Ajoute' => $personne,
-        ]);
-    } */
+
+    /*
+
+        #[Route('/add', name: 'app_personne')]
+        public function addPersonne(ManagerRegistry$doctrine ): Response
+        {
+            //$this->getDoctrine():version Sf<=5
+
+          $entityManager = $doctrine->getManager();
+
+            //Ajouter operation d'insertion dans ma transaction
+            $entityManager->persist($personne);
+            //Execute la transaction
+            $entityManager->flush();
+            return $this->render('personne/details.html.twig', [
+                'personne_Ajoute' => $personne,
+            ]);
+        } */
 }
